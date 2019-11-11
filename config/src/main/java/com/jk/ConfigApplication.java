61470cc0775a29d2d75f0c990a71ffca487e1df1
yxl521 <yuanxiaolong1234@qq.com>
2019-11-11 10:08:11 +0800

    config

--- config/src/main/java/com/jk/ConfigApplication.java
+++ config/src/main/java/com/jk/ConfigApplication.java
@@ -2,15 +2,16 @@ package com.jk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaClient
-@EnableFeignClients
-public class ConsumerFeignApplication {
+public class ConfigApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerFeignApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
 }
